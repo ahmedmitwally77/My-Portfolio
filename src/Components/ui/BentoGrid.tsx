e3bb4db0403808f1c56ebdfd18ec9654f9a7b103
ahmedmitwally77 <anaahmedmito5@gmail.com>
2025-04-16 11:50:53 +0200

    up Bento Grid

--- src/Components/ui/BentoGrid.tsx
+++ src/Components/ui/BentoGrid.tsx
@@ -33,8 +33,6 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
   id,
   img,
   imgClassName,
@@ -44,8 +42,6 @@ export const BentoGridItem = ({
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
   id: number;
   img?: string;
   imgClassName?: string;
