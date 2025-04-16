b3297050b772de499ea15ce226c46d3454ce102b
ahmedmitwally77 <anaahmedmito5@gmail.com>
2025-04-16 12:25:53 +0200

    up Bento Grid6

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { SpotlightPreview } from "../SpotlightPreview/SpotlightSec";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-"use client";
-import Grid from "@/Components/Grid/Grid";
-import Hero from "@/Components/Hero/Hero";
+import dynamic from "next/dynamic";
 
+const Hero = dynamic(() => import("@/Components/Hero/Hero"), { ssr: false });
+const Grid = dynamic(() => import("@/Components/Grid/Grid"), { ssr: false });
 export default function Home() {
   return (
       <div className="w-full h-full text-white">
